24c5a632a574d664c4fb414dd00348d751377754
Ary Davani <arydavani@gmail.com>
2017-08-08 09:56:49 -0400

    added footer

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ $(function(){
 
 // scroll functions
   $(window).scroll(function() {
-    if ($(this).scrollTop() > $('#header').height()) {
+    if ($(this).scrollTop() > $('#header').height() - 10) {
       $('#navbar').css('visibility', 'visible');
-      $('#navbar').fadeIn('fast');
+      $('#navbar').fadeIn(300);
     } else {
-      $('#navbar').fadeOut('fast');
+      $('#navbar').fadeOut(300);
     }
   });
-
 
   $('a[href^="#"]').click(function(){
     var the_id = $(this).attr("href");
     $('html, body').animate({
         scrollTop:$(the_id).offset().top
-    }, 1000);
+    }, 700);
     return false;
   });
+
+
 
   });
